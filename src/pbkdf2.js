d0bfb61fa9aff1a185aeb04a18116625b9d9a30a
Jeff.Mott.OR <Jeff.Mott.OR@eba437e6-77a4-11de-b799-7765b5330b87>
2011-05-13 03:45:47 +0000

    Refactoring and cosmetic changes.
    git-svn-id: http://crypto-js.googlecode.com/svn/branches/3.x@335 eba437e6-77a4-11de-b799-7765b5330b87

---
+++ src/pbkdf2.js
@@ -0,0 +1,61 @@
+(function (C) {
+    // Shortcuts
+    var BaseObj = C.oop.BaseObj;
+    var WordArray_Hex = C.lib.WordArray.Hex;
+
+    var PBKDF2 = C.PBKDF2 = BaseObj.extend({
+        // Config defaults
+        cfg: BaseObj.extend({
+            keySize: 4,
+            hasher: C.SHA1,
+            iterations: 1
+        }),
+
+        compute: function (password, salt, cfg) {
+            // Apply config defaults
+            cfg = this.cfg.extend(cfg);
+
+            // Init HMAC
+            var hmac = C.HMAC.create(cfg.hasher, password);
+
+            // Initial values
+            var derivedKey = WordArray_Hex.create();
+            var blockIndex = WordArray_Hex.create([1]);
+
+            // Shortcuts
+            var derivedKeyWords = derivedKey.words;
+            var blockIndexWords = blockIndex.words;
+            var keySize = cfg.keySize;
+            var iterations = cfg.iterations;
+
+            // Generate key
+            while (derivedKeyWords.length < keySize) {
+                var block = hmac.update(salt).compute(blockIndex);
+
+                // Shortcuts
+                var blockWords = block.words;
+                var blockWordsLength = blockWords.length;
+
+                // Iterations
+                var intermediate = block;
+                for (var i = 1; i < iterations; i++) {
+                    intermediate = hmac.compute(intermediate);
+
+                    // Shortcuts
+                    var intermediateWords = intermediate.words;
+
+                    // XOR block with intermediate
+                    for (var j = 0; j < blockWordsLength; j++) {
+                        blockWords[j] ^= intermediateWords[j];
+                    }
+                }
+
+                derivedKey.concat(block);
+                blockIndexWords[0]++;
+            }
+            derivedKey.sigBytes = keySize * 4;
+
+            return derivedKey;
+        }
+    });
+}(CryptoJS));
